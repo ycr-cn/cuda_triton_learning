34b6ce5cfa78ee93fe942ef6a6fc00618772a10e
ycr_server <ycryes@163.com>
2026-04-04 13:06:36 +0000

    readability optimation

--- single_operators/vector_add/vector_add_cuda_03.cu
+++ single_operators/vector_add/vector_add_cuda_03.cu
@@ -4,23 +4,48 @@
 #define BLOCK_SIZE 256
 #define GRID_SIZE 96
 
-__global__ void vector_add(const float* x, const float* y, float* out, const int N){
-    int stride = gridDim.x * blockDim.x * 4;
-    int idx = (blockIdx.x * blockDim.x + threadIdx.x) * 4;
+// __global__ void vector_add(const float* x, const float* y, float* out, const int N){
+//     int stride = gridDim.x * blockDim.x * 4;
+//     int idx = (blockIdx.x * blockDim.x + threadIdx.x) * 4;
 
-    for(int i = idx; i < N; i += stride){
-        if(i + 3 < N){ 
-            float4 a = *reinterpret_cast<const float4*>(&x[i]);
-            float4 b = *reinterpret_cast<const float4*>(&y[i]);
-            float4 c = make_float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
-            *reinterpret_cast<float4*>(&out[i]) = c;
-        }
-        else{
-            while(i < N){
-                out[i] = x[i] + y[i];
-                ++i;
-            }
-        }
+//     for(int i = idx; i < N; i += stride){
+//         if(i + 3 < N){ 
+//             float4 a = *reinterpret_cast<const float4*>(&x[i]);
+//             float4 b = *reinterpret_cast<const float4*>(&y[i]);
+//             float4 c = make_float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
+//             *reinterpret_cast<float4*>(&out[i]) = c;
+//         }
+//         else{
+//             while(i < N){
+//                 out[i] = x[i] + y[i];
+//                 ++i;
+//             }
+//         }
+//     }
+// }
+
+__global__ void vector_add(const float* x, const float* y, float* out, int N) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    const float4* x4 = reinterpret_cast<const float4*>(x);
+    const float4* y4 = reinterpret_cast<const float4*>(y);
+    float4* out4 = reinterpret_cast<float4*>(out);
+
+    int N4 = N / 4;
+
+    for (int i = tid; i < N4; i += stride) {
+        float4 a = x4[i];
+        float4 b = y4[i];
+        out4[i] = make_float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
+    }
+
+    int tail = N % 4;
+    int tail_start = N4 * 4;
+
+    if (tid < tail) {
+        int i = tail_start + tid;
+        out[i] = x[i] + y[i];
     }
 }
 
